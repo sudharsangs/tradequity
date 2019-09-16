8cbae1f84e3045d9e11ead0c89f080dee4fd7ccb
sudharsangs <g.s.sudharsan99@gmail.com>
2019-09-16 19:46:30 +0530

    Updating Live Share

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import logo from './logo1.png';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
 import Navbar from "./components/navbar.component";
 import LogIn from "./components/login.component";
 import SignUp from "./components/signup.component";
 import LivePrice from "./components/live.component";
+import Predict from "./components/predict.component"
 
 function App() {
   return (
-    <Router>
+
+      <Router>
       <Navbar />
       <br/>
       <Route path="/live" exact component={LivePrice} />
-      <Route path="/signup" exact component={SignUp} />
+      <Route path="/signup" exact component={SignUp} />   
+
       <Route path="/login" exact component={LogIn} />
-    </Router>
+      <Route path="/predict" exact component={Predict} />
+      </Router>
+    
+   
   );
 }
 
---
+++ frontend/src/components/predict.component.js
@@ -0,0 +1,36 @@
+import React,{ Component } from 'react';
+import { MDBContainer, MDBRow, MDBCol, MDBDropdown,MDBDropdownToggle,MDBDropdownMenu,MDBDropdownItem, MDBBadge } from 'mdbreact';
+
+export default class Predict extends Component
+{
+    render(){
+        return (
+            <div>
+             <br/>
+            <br/>
+            <MDBContainer>
+              <MDBRow>
+                <MDBCol md="6">
+                <h2><MDBBadge color="warning">Choose from below to see predict share price</MDBBadge></h2>
+                <br/>
+                <br/>
+                    
+                        <MDBDropdown>
+                             <MDBDropdownToggle caret color="secondary" >
+                               Live Share Price
+                             </MDBDropdownToggle>
+                             <MDBDropdownMenu basic >
+                              <MDBDropdownItem>Maruti Suzuki</MDBDropdownItem>
+                              <MDBDropdownItem>Adani Gas Limited</MDBDropdownItem>
+                              <MDBDropdownItem>Allahabad Bank</MDBDropdownItem>
+                              <MDBDropdownItem>Bajaj Auto Limited</MDBDropdownItem>
+                             </MDBDropdownMenu>
+                        </MDBDropdown>
+                        
+                </MDBCol>
+               </MDBRow>
+               </MDBContainer>
+               </div>
+        )
+    }
+}
