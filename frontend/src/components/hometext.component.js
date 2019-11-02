bdb4c9aef3c4bbd6a3556dfe022c50c71bcf59a3
ParikshithV <56472103+ParikshithV@users.noreply.github.com>
2019-11-02 10:24:59 +0530

    Added Home Text

--- frontend/src/components/hometext.component.js
+++ frontend/src/components/hometext.component.js
@@ -4,9 +4,20 @@ class Home extends Component
 {
     render(){
         return (
-            <div>
-                <p>This Project is used to predict Stock Prices</p>
-            </div>
+           <MDBContainer>
+            <MDBRow>
+              <MDBCol md="6">
+              <MDBCard>
+              <MDBCardBody>
+                  <h1>Tradequity Trading App</h1>
+                  <h2>A one stop solution to manage your stocks</h2>
+                  <p>Don't worry about privacy. Admins don't have have any acceess to the data you store in our site</p>
+                  <h2>#We Support Privacy</h2>
+               </MDBCardBody>
+               </MDBCard>
+             </MDBCol>
+            </MDBRow>
+            </MDBContainer>
         )
     }
 }
